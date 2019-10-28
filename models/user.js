f42f344eb91f42dbad7127db3ac450443a296dab
John Pitsakis <54716986+slb19@users.noreply.github.com>
2019-10-28 02:08:35 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -12,15 +12,6 @@ var userSchema= new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose);
 
-/*
-userSchema.statics.findByUsername=async(username)=>{
-    
-    const User=await user.findOne({username})
-    if(!User){
-        throw new Error("There is no such username.Please Sign-up")
-    }
-}
-*/
 
 var user=mongoose.model("user",userSchema);
 
