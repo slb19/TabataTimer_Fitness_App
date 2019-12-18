75e9816432e2e7a7cb28b8859bb8adb85831ac49
John Pitsakis <54716986+slb19@users.noreply.github.com>
2019-12-18 11:05:12 +0200

    Update tabataAppF.js

--- public/tabataAppF.js
+++ public/tabataAppF.js
@@ -1,259 +1,100 @@
-var secondsW=20;
-var initialW=20
-var secondsR=10;
-var initialR=10;
-var secondsPr=5;
-var initialPr=5;
-var rest=10;
-var roundsNum=2;
-var initialRN=2
-var tabatas=2;
-var initialT=2;
-var startWorkout;
-var startrest;
-var workoutWindow;
-var startprepare;
-var pauseevent=0;
-var whistlecontrol=2;
-var restControl=2;
-
-const getready=document.getElementById("c1");
-const setworkout=document.getElementById("c2");
-const setrest=document.getElementById("c3");
-const setrounds=document.getElementById("c4");
-const settabatas=document.getElementById("c5");
-
-const controls1=document.querySelectorAll(".controls1 span");
-//console.log(controls1);
-
-var getreadycolor=getready.style.backgroundColor;
-var setworkoutcolor=setworkout.style.backgroundColor;
-var setrestcolor=setrest.style.backgroundColor;
-var setroundscolor=setrounds.style.backgroundColor;
-var settabatascolor=settabatas.style.backgroundColor;
+let secondsW=20;
+let initialW=20
+let secondsR=10;
+let initialR=10;
+let secondsPr=5;
+let initialPr=5;
+let rest=10;
+let roundsNum=2;
+let initialRN=2
+let tabatas=2;
+let initialT=2;
+let startWorkout;
+let startrest;
+let workoutWindow;
+let startprepare;
+let pauseevent=0;
+let whistlecontrol=2;
+let restControl=2;
 
 const whistle= new Audio("Whistle.wav");
 const restTime=new Audio("/Rest.mp3");
 
-//GET READY CONTROLS..................................................................................................
-function removelistenerGetr(){
-  controls1[0].removeEventListener("click",plus);
-  controls1[1].removeEventListener("click",minus);
-}
+//CONTROLS..............................................................................
+const divs=document.querySelectorAll(".c");
+const controls1=document.querySelectorAll(".controls1 span");
 
+divs.forEach(el=>el.addEventListener("click", getControls));
 
-getready.addEventListener("click",Getr);
-
-function Getr(e){
-  if (setworkoutcolor==="" && setrestcolor==="" && setroundscolor==="" && settabatascolor===""){
-if(getreadycolor===""){
-  console.log(e.target);
-  getready.style.backgroundColor="#3C8747";
-  getreadycolor=getready.style.backgroundColor;
-  
- controls1[0].addEventListener("click",window.plus=function plus() {
-  document.getElementById("c1.1").innerHTML=++secondsPr;
-  initialPr=secondsPr;
-  });
-
- controls1[1].addEventListener("click",window.minus=function minus() {
-  if(secondsPr<=1) return secondsPr=1;
-  document.getElementById("c1.1").innerHTML=--secondsPr;    
-  initialPr=secondsPr;
-  });
-}
-
-else if(getreadycolor="#3C8747"){
-  getready.style.backgroundColor="";
-  getreadycolor=getready.style.backgroundColor;
-console.log(plus);
-removelistenerGetr();
+const controlsApp=(id, value)=>{
+  switch(id){
+    case "c1":
+       secondsPr=value
+       initialPr=secondsPr
+       break;
+    case "c2":
+       document.getElementById("workout-seconds").innerHTML=value;
+       secondsW=value;
+       initialW=secondsW
+       break;
+    case "c3":
+       secondsR=value
+       initialR=secondsR;
+       break;
+    case "c4":
+       document.getElementById("rounds-window").innerHTML=value;
+       roundsNum=value
+       initialRN=roundsNum;
+       break;
+       case"c5":
+       document.getElementById("noOfTabatas-window").innerHTML=value;
+       tabatas=value;
+       initialT=tabatas
+    }
  }
+
+function getControls(e){
+ let otherDivs=Array.from(divs).filter(div=>div.id!=e.target.id)
+
+     if(e.target.style.backgroundColor===""){
+           e.target.style.backgroundColor="#3C8747"
+             otherDivs.forEach(div=>div.removeEventListener("click", getControls))
+
+             controls1[0].addEventListener("click",window.plus=function plus() {
+             let value=e.target.childNodes[1].textContent;
+             e.target.childNodes[1].textContent=++value;
+             controlsApp(e.target.id, value);
+           });
+
+             controls1[1].addEventListener("click",window.minus=function minus() {
+              if(e.target.childNodes[1].textContent>1){
+                  let value=e.target.childNodes[1].textContent;
+                  e.target.childNodes[1].textContent=--value;
+             
+                controlsApp(e.target.id, value);  
+             } 
+           });
+     }else {
+       e.target.style.backgroundColor=""
+           otherDivs.forEach(div=>div.addEventListener("click", getControls))
+           removeListenerGetr()
+     }
 }
-else{
-  getready.removeEventListener("click",Getr);
-  getready.addEventListener("click",Getr);
-}
-}
 
-//WORKOUT CONTROLS.........................................................................................
-function removelistenerSetw(){
-  controls1[0].removeEventListener("click",plusw);
-  controls1[1].removeEventListener("click",minusw);
-}
-
-
-setworkout.addEventListener("click",Setw);
-function Setw(e){
-  if(getreadycolor==="" && setrestcolor==="" && setroundscolor==="" && settabatascolor===""){
-if(setworkoutcolor===""){
-  
-  setworkout.style.backgroundColor="#3C8747";
-  setworkoutcolor=setworkout.style.backgroundColor;
-
- controls1[0].addEventListener("click",window.plusw=function plusw() {
-  document.getElementById("c1.2").innerHTML=++secondsW;
-  document.getElementById("workout-seconds").innerHTML=secondsW;
-  initialW=secondsW;
-  });
- 
-  
-
- controls1[1].addEventListener("click",window.minusw=function minusw() {
-  if(secondsW<=1) return secondsW=1;
-  document.getElementById("c1.2").innerHTML=--secondsW;
-  document.getElementById("workout-seconds").innerHTML=secondsW;
-  initialW=secondsW;
-  });
+function removeListenerGetr(){
+     controls1[0].removeEventListener("click",plus);
+     controls1[1].removeEventListener("click",minus);
+   }
 
  
-}
-else if(setworkoutcolor="#3C8747"){
-  setworkout.style.backgroundColor="";
-  setworkoutcolor=setworkout.style.backgroundColor;
-console.log(plusw);
-removelistenerSetw();
- }
-}
- else{
-  setworkout.removeEventListener("click",Setw);
-  setworkout.addEventListener("click",Setw);
- }
-}
-
-//REST CONTROLS................................................................................................
-function removelistenerSetr(){
-  controls1[0].removeEventListener("click",plusr);
-  controls1[1].removeEventListener("click",minusr);
-}
-
-
-setrest.addEventListener("click",Setr);
-function Setr(e){
-  if(getreadycolor==="" && setworkoutcolor==="" && setroundscolor==="" && settabatascolor===""){
-if(setrestcolor===""){
-  
-  setrest.style.backgroundColor="#3C8747";
-  setrestcolor=setrest.style.backgroundColor;
-
- controls1[0].addEventListener("click",window.plusr=function plusr() {
-  document.getElementById("c1.3").innerHTML=++secondsR;
-  initialR=secondsR;
-  });
-
- controls1[1].addEventListener("click",window.minusr=function minusr() {
-  if(secondsR<=1) return secondsR=1;
-  document.getElementById("c1.3").innerHTML=--secondsR;
-  initialR=secondsR;
-  });
-}
-else if(setrestcolor="#3C8747"){
-  setrest.style.backgroundColor="";
-  setrestcolor=setworkout.style.backgroundColor;
-console.log(plusr);
-removelistenerSetr();
- }
-}
- else{
-  setrest.removeEventListener("click",Setr);
-  setrest.addEventListener("click",Setr);
- }
-}
-
-//ROUNDS CONTROLS.........................................................................................
-function removelistenerSetrou(){
-  controls1[0].removeEventListener("click",plusrou);
-  controls1[1].removeEventListener("click",minusrou);
-}
-
-
-setrounds.addEventListener("click",Setrou);
-function Setrou(e){
-  if(getreadycolor==="" && setworkoutcolor==="" && setrestcolor==="" && settabatascolor===""){
-if(setroundscolor===""){
-  
-  setrounds.style.backgroundColor="#3C8747";
-  setroundscolor=setrounds.style.backgroundColor;
-
- controls1[0].addEventListener("click",window.plusrou=function plusrou() {
-  document.getElementById("c1.4").innerHTML=++roundsNum;
-  document.getElementById("rounds-window").innerHTML=roundsNum;
-  initialRN=roundsNum;
-  });
-
- controls1[1].addEventListener("click",window.minusrou=function minusrou() {
-  if(roundsNum<=1) return roundsNum=1;
-  document.getElementById("c1.4").innerHTML=--roundsNum;
-  document.getElementById("rounds-window").innerHTML=roundsNum;
-  initialRN=roundsNum;
-  });
-}
-else if(setroundscolor="#3C8747"){
-  setrounds.style.backgroundColor="";
-  setroundscolor=setrounds.style.backgroundColor;
-console.log(plusrou);
-removelistenerSetrou();
- }
-}
- else{
-  setrest.removeEventListener("click",Setr);
-  setrest.addEventListener("click",Setr);
- }
-}
-
-//No oF Tabatas CONTROLS.............................................................................................
-function removelistenerSettab(){
-  controls1[0].removeEventListener("click",plust);
-  controls1[1].removeEventListener("click",minust);
-}
-
-
-settabatas.addEventListener("click",Settab);
-function Settab(e){
-  if(getreadycolor==="" && setworkoutcolor==="" && setrestcolor==="" && setroundscolor===""){
-if(settabatascolor===""){
-  
-  settabatas.style.backgroundColor="#3C8747";
-  settabatascolor=settabatas.style.backgroundColor;
-
- controls1[0].addEventListener("click",window.plust=function plust() {
-  document.getElementById("c1.5").innerHTML=++tabatas;
-  document.getElementById("noOfTabatas-window").innerHTML=tabatas;
-  initialT=tabatas
-  });
-
- controls1[1].addEventListener("click",window.minust=function minust() {
-  if(tabatas<=1) return tabatas=1;
-  document.getElementById("c1.5").innerHTML=--tabatas;
-  document.getElementById("noOfTabatas-window").innerHTML=tabatas;
-  
-  initialT=tabatas;
-  });
-}
-else if(settabatascolor="#3C8747"){
-  settabatas.style.backgroundColor="";
-  settabatascolor=settabatas.style.backgroundColor;
-console.log(plust);
-removelistenerSettab();
- }
-}
- else{
-  settabatas.removeEventListener("click",Setab);
-  settabatas.addEventListener("click",Setab);
- }
-}
-
 //APP..............................................................................................
-
-
 const buttonGo= document.getElementById("go-btn");
 const buttonsStopPause=document.querySelectorAll(".hide-btn");
 const rounds=document.getElementById("rounds-window");
 const stopButton=document.querySelector(".buttons span:nth-child(1)");
 const pauseButton=document.querySelector(".buttons span:nth-child(3)");
-console.log(pauseButton);
+//console.log(pauseButton);
 const noOfTabatas=document.getElementById("noOfTabatas-window");
-
 
 function reset(){
   workoutWindow=document.querySelector("#workout-window div");
